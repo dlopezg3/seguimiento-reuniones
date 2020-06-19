8e858c8eecb1b3cd5f8eb9fddecc80b1e915e5c8
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-19 18:40:02 -0500

    Regex para la validación de correos en formularios

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,5 +1,7 @@
 class Assistance < ApplicationRecord
   belongs_to :meet
 
-  validates :email, presence: true
+  validates :email, presence: true, format: {
+                                      with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+                                      :message => 'no válido' }
 end
