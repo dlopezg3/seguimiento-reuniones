262231591a3fa487d44dd82e0c9d29db5f648d95
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-23 17:15:59 -0500

    Crear relación entre tareas y assistentes

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,5 +1,6 @@
 class Assistance < ApplicationRecord
   belongs_to :meet
+  has_many :tasks
 
   validates :email, presence: true, format: {
                                       with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
