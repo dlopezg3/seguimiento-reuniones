67b5919b7a2450d551417f1617b6da41a4f0fe45
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-25 15:33:19 -0500

    Url para hacer llamado de api post y crear tareas en planner

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  namespace :meets do
+    resources :excel_rows, only: :index
+  end
+
   resources :meets do
     resources :assistances, only: %i[new create]
     resources :tasks, only: %i[new create]
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
