016d54604c286a920ee4182db0273623448ed592
Daniel Lopez <dlopez.10g@gmail.com>
2020-06-25 15:35:03 -0500

    helper files new controller

---
+++ app/helpers/meets/excel_rows_helper.rb
@@ -0,0 +1,2 @@
+module Meets::ExcelRowsHelper
+end
---
+++ test/controllers/meets/excel_rows_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Meets::ExcelRowsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
