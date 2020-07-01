134e071e95c61ba569bb753d8b8b4f0d88925e8d
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-01 10:52:57 -0500

    Rich text para agenda y protocolo de la reunión

--- app/models/meet.rb
+++ app/models/meet.rb
@@ -3,6 +3,8 @@ class Meet < ApplicationRecord
   has_many :tasks, through: :assistances
 
   has_rich_text :notes
+  has_rich_text :protocol
+  has_rich_text :agenda
 
   validates :nombre, presence: true
 end
