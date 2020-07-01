ec5ae3f96c570204c6615b9a09111d962a0ce077
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-01 09:37:14 -0500

    ruta url para cración de planes en planner

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :meets do
     resources :assistances, only: %i[new create]
     resources :tasks, only: %i[new create]
+    resources :plans, only: :create, defaults: {format: 'js'}, controller: "meets/plans"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
