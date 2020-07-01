bd65efd3c2d14423ba4139340889dfd246291a91
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-01 16:44:00 -0500

    Referencia de tareas con secreataría y validaciones

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   has_and_belongs_to_many :assistances
-  # belongs_to :assistance
+  belongs_to :secretary
+
+  validates :assistances, presence: true
+  validates :nombre, presence: true
 end
