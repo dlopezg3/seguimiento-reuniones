74c562d7a12114370b117245b00531a11753e750
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-01 09:33:07 -0500

    Modifación en la relación de tareas y asistentes.  Una tarea pertenece y a uno o a varios asistentes

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,6 +1,6 @@
 class Assistance < ApplicationRecord
   belongs_to :meet
-  has_many :tasks
+  has_and_belongs_to_many :tasks
 
   validates :email, presence: true, format: {
                                       with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
